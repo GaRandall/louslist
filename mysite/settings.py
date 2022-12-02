c09defe86a04c74b4e401962b17a96bb78888599
Ji <chen.jikai44@gmail.com>
2022-12-02 02:59:14 -0500

    Security Update
    I apologize for all the confusion and reverts. I panicked and did not clear my session cookies and cache when I messed up earlier on testing the HTTPS on a non-development server. This is the final one.

--- mysite/settings.py
+++ mysite/settings.py
@@ -173,3 +173,13 @@ try:
         django_heroku.settings(locals())
 except ImportError:
     found = False
+
+if 'runserver' in sys.argv:
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+else:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
