7521703b69abdd1d9bfb8aab9d8c645eb83c1839
Ji <chen.jikai44@gmail.com>
2022-12-02 00:56:20 -0500

    Security Measure Implemented

--- mysite/settings.py
+++ mysite/settings.py
@@ -173,3 +173,7 @@ try:
         django_heroku.settings(locals())
 except ImportError:
     found = False
+
+# Security Measures
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
